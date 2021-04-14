f84f5649592ccea1bf270a8be27c9565eba1854a
zkan <zkan.cs@gmail.com>
2021-04-14 15:52:40 +0700

    Use default args for all tasks :bear:

--- astro/dags/simple_dag.py
+++ astro/dags/simple_dag.py
@@ -1,10 +1,27 @@
+from datetime import timedelta
+
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
-
+default_args = {
+    'retry': 5,
+    'retry_delay': timedelta(minutes=5)
+}
 with DAG(dag_id="simple_dag",
+         default_args=default_args,
          schedule_interval="@daily",
          start_date=days_ago(3),
          catchup=False) as dag:
-    task_1 = DummyOperator(task_id="task_1")
+    
+    task_1 = DummyOperator(
+        task_id="task_1",
+    )
+
+    task_2 = DummyOperator(
+        task_id="task_2",
+    )
+
+    task_3 = DummyOperator(
+        task_id="task_3",
+    )
