8b1cf8379c7d9774ec669a12583599e8d7d359fd
zkan <zkan.cs@gmail.com>
2021-04-14 16:17:22 +0700

    Try FileSensor to check if file exists :bear:

--- astro/dags/simple_dag.py
+++ astro/dags/simple_dag.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
+from airflow.sensors.filesystem import FileSensor
 from airflow.utils.dates import days_ago
 
 
@@ -11,6 +12,9 @@ def _downloading_data(my_param, ds, **kwargs):
     print(my_param)
     print(ds)
     print(kwargs)
+
+    with open("/tmp/my_file.txt", "w") as f:
+        f.write("my_data")
 
 
 default_args = {
@@ -39,4 +43,11 @@ with DAG(dag_id="simple_dag",
         task_id="downloading_data",
         python_callable=_downloading_data,
         op_kwargs={"my_param": 42},
+    )
+
+    waiting_for_data = FileSensor(
+        task_id="waiting_for_data",
+        fs_conn_id="fs_default",
+        filepath="my_data.txt",
+        poke_interval=5,
     )
