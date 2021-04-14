ef20b034515860abce9866a71d96d81a109953e1
zkan <zkan.cs@gmail.com>
2021-04-14 15:12:58 +0700

    Add a dummy task and schedule DAG :bear:

--- astro/dags/simple_dag.py
+++ astro/dags/simple_dag.py
@@ -1,5 +1,9 @@
 from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.utils import timezone
 
 
-with DAG(dag_id='simple_dag') as dag:
-    None
+with DAG(dag_id="simple_dag",
+         schedule_interval="*/10 * * * *",
+         start_date=timezone.datetime(2021, 1,1 )) as dag:
+    task_1 = DummyOperator(task_id="task_1")
