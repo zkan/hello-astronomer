3e27651754841c9f0c1e07abe299502cc1c2f75a
zkan <zkan.cs@gmail.com>
2021-04-14 15:22:14 +0700

    Set start_date to 3 days ago and set catchup to False :bear:

--- astro/dags/simple_dag.py
+++ astro/dags/simple_dag.py
@@ -1,9 +1,10 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from airflow.utils import timezone
+from airflow.utils.dates import days_ago
 
 
 with DAG(dag_id="simple_dag",
-         schedule_interval="*/10 * * * *",
-         start_date=timezone.datetime(2021, 1,1 )) as dag:
+         schedule_interval="@daily",
+         start_date=days_ago(3),
+         catchup=False) as dag:
     task_1 = DummyOperator(task_id="task_1")
